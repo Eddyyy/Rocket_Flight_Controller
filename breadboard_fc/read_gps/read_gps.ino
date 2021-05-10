7b54285cd96d4e88c4d4911db27c652077d1dd6e
kevin hu <kevinhu12@hotmail.com>
2021-05-10 22:14:47 +1000

    Basic GPS module readouts

--- breadboard_fc/read_gps/read_gps.ino
+++ breadboard_fc/read_gps/read_gps.ino
@@ -4,25 +4,37 @@
 // The TinyGPS++ object
 TinyGPSPlus gps;
 
-void setup()
-{
-  Serial.begin(115200);
-  Serial2.begin(9600);
-  while(!Serial){
-      ;
-  }
+void setup() {
+    Serial.begin(115200);
+    Serial2.begin(9600);
+    while(!Serial){
+    ;   // wait for serial port to connect.
+    }
 }
 
-void loop()
-{
-  // Dispatch incoming characters
-  while (Serial2.available())
-    Serial.print(Serial2.readStringUntil("\n"));
+void loop() {
+    while (Serial2.available()) {
+        gps.encode(Serial2.read());
+    }
+    if (gps.location.isUpdated()) {
+        Serial.print(F("Lat ="));
+        Serial.print(gps.location.lat(), 6);
+        Serial.print(F("Long ="));
+        Serial.println(gps.location.lng(), 6);
+    } else if (gps.speed.isUpdated()) {
+        Serial.print(F("Speed (m/s) ="));
+        Serial.print(gps.speed.mps());
+    } else if (gps.course.isUpdated()) {
+        Serial.print(F("Course (Deg) ="));
+        Serial.println(gps.course.deg());
+    } else if (gps.altitude.isUpdated()) {
+        Serial.print(F("Altitude (m) ="));
+        Serial.print(gps.altitude.meters());
+    } else if (gps.satellites.isUpdated()) {
+        Serial.print(F("Satellites ="));
+        Serial.println(gps.satellites.value());
+    } else if (gps.hdop.isUpdated()) {
+        Serial.print(F("HDOP ="));
+        Serial.println(gps.hdop.hdop());
+    }
 }
-
-//speed
-//alt
-//location
-//course
-//hdop
-//satalite
