3f9dceb4a8d1f2f0ddc2e0f4189dea3fa4feab93
AnonymousAlligator <kevinhu12@hotmail.com>
2021-05-10 15:11:41 +1000

    GPS reading dev

---
+++ breadboard_fc/read_gps/read_gps.ino
@@ -0,0 +1,28 @@
+#include "TinyGPS++.h"
+#include <SoftwareSerial.h>
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial2.begin(9600);
+  while(!Serial){
+      ;
+  }
+}
+
+void loop()
+{
+  // Dispatch incoming characters
+  while (Serial2.available)
+    Serial.print(Serial2.read);
+}
+
+//speed
+//alt
+//location
+//course
+//hdop
+//satalite
